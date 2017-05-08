2e44f8e714f80b1d915e47207ddeec96f816aedb
Lachlan Ross <lachlan.ross@salesforce.com>
2017-05-08 18:39:50 +1000

    no message

--- demo.php
+++ demo.php
@@ -1,22 +1,23 @@
 <?php
 
-$request = new HttpRequest();
-$request->setUrl('https://android.googleapis.com/gcm/send');
-$request->setMethod(HTTP_METH_POST);
+$client = new http\Client;
+$request = new http\Client\Request;
+
+$body = new http\Message\Body;
+$body->append('{"registration_ids":["d2ggGiTEChY:APA91bEhk6GPkX7OI13ZbBPSlb3TCMnXiBeylPspTJoaudPAwmxF8ngvE9LhyW7sC8g3S0gY4Vtf5YSWM0KV-WhayvYzkmF6rVYtkD9kDoR5CyQKygQfKxWp0W286HjcmiOaEjyclG-N"]}');
+
+$request->setRequestUrl('https://android.googleapis.com/gcm/send');
+$request->setRequestMethod('POST');
+$request->setBody($body);
 
 $request->setHeaders(array(
-  'postman-token' => 'f7555a09-3e7c-7f13-be40-8d3616dbbff1',
+  'postman-token' => '5da42373-c49d-39b8-f77f-91c384255425',
   'cache-control' => 'no-cache',
   'content-type' => 'application/json',
   'authorization' => 'key=AAAAEzBWVWU:APA91bH8wdRRiOtqdSAfLjrXlb1jmuvr_UExaeI9QDdjdiop_nSPHXUX0cfM-khb1qcm8V9uV61BLhibMxjkgeOHLtKMp2Z7zG8PMLO4iBZVx2SQ8jAKVia20RSL4nPEdhHZMkKbDrCYkxRJG5vX5T8DQPzFEunLXg'
 ));
 
-$request->setBody('{"registration_ids":["d2ggGiTEChY:APA91bEhk6GPkX7OI13ZbBPSlb3TCMnXiBeylPspTJoaudPAwmxF8ngvE9LhyW7sC8g3S0gY4Vtf5YSWM0KV-WhayvYzkmF6rVYtkD9kDoR5CyQKygQfKxWp0W286HjcmiOaEjyclG-N"]}');
+$client->enqueue($request)->send();
+$response = $client->getResponse();
 
-try {
-  $response = $request->send();
-
-  echo $response->getBody();
-} catch (HttpException $ex) {
-  echo $ex;
-}
+echo $response->getBody();
