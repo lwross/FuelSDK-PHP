9421e51d4166085e4c6f56e4cf8969845a43e202
Lachlan Ross <lachlan.ross@salesforce.com>
2017-05-28 14:49:09 +1000

    strattona algorithm

---
+++ stratton_next_best_offer.php
@@ -0,0 +1,32 @@
+<?php
+
+ if ($_REQUEST['LegalEntityType'] == "Company") {
+?>
+  
+  <div class="intro" style="box-sizing: border-box; margin-bottom: 20px; font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);">
+<h1 style="box-sizing: border-box; margin: 10px 0px; font-size: 30px; font-family: Oswald, sans-serif; font-weight: bolder; line-height: 32px; color: inherit; text-transform: uppercase; padding-top: 5px;">BUSINESS&nbsp;<span class="highlight" style="box-sizing: border-box; color: rgb(90, 60, 145);">CAR FINANCE</span>&nbsp;&amp; BUSINESS&nbsp;<span class="highlight" style="box-sizing: border-box; color: rgb(90, 60, 145);">CAR LOANS</span></h1>
+<div class="description" style="box-sizing: border-box; font-family: Oswald, sans-serif; text-transform: uppercase; font-size: 12px; margin-top: -10px;">WE HAVE THE RIGHT&nbsp;<span style="box-sizing: border-box; font-weight: 700;">SOLUTION</span>&nbsp;FOR YOUR NEEDS.</div>
+</div>
+<section style="box-sizing: border-box; font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);">
+<p style="box-sizing: border-box; margin: 0px 0px 10px;">Companies, partnerships &amp; sole traders looking for business car finance or a business car loan have a range of options available to them. The most appropriate type of finance for each business&#39; needs will depend on business structure, cash-flow requirements, GST registration status and tax considerations.&nbsp;<span style="box-sizing: border-box; font-weight: 700;">stratton</span>&nbsp;specialise in car finance for companies, partnerships, sole traders and other businesses - we have the right solution for your needs.</p>
+</section>
+
+  
+<?php
+
+ } else {
+ ?>
+  
+<div class="intro" style="box-sizing: border-box; margin-bottom: 20px; font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);">
+<div class="intro" style="box-sizing: border-box; margin-bottom: 20px; font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);">
+<h1 style="box-sizing: border-box; margin: 10px 0px; font-size: 30px; font-family: Oswald, sans-serif; font-weight: bolder; line-height: 32px; color: inherit; text-transform: uppercase; padding-top: 5px;"><span class="highlight" style="box-sizing: border-box; color: rgb(90, 60, 145);">PERSONAL</span>&nbsp;CAR FINANCE &amp;&nbsp;<span class="highlight" style="box-sizing: border-box; color: rgb(90, 60, 145);">PERSONAL</span>&nbsp;CAR LOANS</h1>
+<div class="description" style="box-sizing: border-box; font-family: Oswald, sans-serif; text-transform: uppercase; font-size: 12px; margin-top: -10px;"><span style="box-sizing: border-box; font-weight: 700;">STRATTON</span>&nbsp;SPECIALISE IN&nbsp;<span class="highlight" style="box-sizing: border-box; color: rgb(90, 60, 145);">CAR FINANCE FOR INDIVIDUALS</span>, WE HAVE THE RIGHT SOLUTION FOR YOUR NEEDS</div>
+</div>
+<section style="box-sizing: border-box; font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);">
+<p style="box-sizing: border-box; margin: 0px 0px 10px;">Individuals have a range of personal car finance and personal car loan options available to them, the choice of which will depend on how often you use your vehicle for business, whether your employer offers you salary packaging and the type of car you want to buy.</p>
+</section>
+</div>
+<?php
+
+ } 
+ ?>
