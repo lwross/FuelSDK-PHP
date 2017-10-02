ee60cfc83c6d8a412218502b9383a9671ddf58b9
Lachlan Ross <lachlan.ross@salesforce.com>
2017-10-02 13:49:50 +1100

    no message

--- config.js.php
+++ config.js.php
@@ -2,3 +2,5 @@ window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
   gcmAPIKey: '<?=getenv('GCM_API_KEY');?>'
 };
+
+console.log(window.GoogleSamples.Config);
